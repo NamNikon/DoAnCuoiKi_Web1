441e915a17e6532642b4d7ee3327069752d9ea3c
Nguyen Trung Nam <trungnam47.mc@gmail.com>
2021-12-18 23:17:57 +0700

    database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -40,7 +40,8 @@ class UserFactory extends Factory
      */
     public function unverified()
     {
-        return $this->state(function (array $attributes) {
+        return $this->state(function (array $attributes) 
+        {
             return [
                 'email_verified_at' => null,
             ];
@@ -54,13 +55,15 @@ class UserFactory extends Factory
      */
     public function withPersonalTeam()
     {
-        if (! Features::hasTeamFeatures()) {
+        if (! Features::hasTeamFeatures()) 
+        {
             return $this->state([]);
         }
 
         return $this->has(
             Team::factory()
-                ->state(function (array $attributes, User $user) {
+                ->state(function (array $attributes, User $user) 
+                {
                     return ['name' => $user->name.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
                 }),
             'ownedTeams'
