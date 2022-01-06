e35d9d74b3cf880b553c35caf8a160ad4e71c453
= <ppxxhh111@gmail.com>
2022-01-06 23:53:15 +0700

    add produc liked most

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,65 +10,74 @@ class PagesController extends BaseController
     public function Index()
     {
         $categories = DB::table('categories')
-                            ->join('images', 'images.id', '=', 'categories.logo_image')
-                            ->get();
+            ->join('images', 'images.id', '=', 'categories.logo_image')
+            ->get();
         $orderDetail = DB::table('order_details')
-                            ->select('product_id', DB::raw('SUM(quantity) as total'))
-                            ->groupBy('product_id')
-                            ->orderBy('total', 'desc')
-                            ->take(10);
+            ->select('product_id', DB::raw('SUM(quantity) as total'))
+            ->groupBy('product_id')
+            ->orderBy('total', 'desc')
+            ->take(10);
         $topSaleProducts = DB::table('products')
-                            ->joinSub($orderDetail, 'order_detail', function($join){
-                                $join->on('products.id', '=', 'order_detail.product_id');
-                            })
-                            ->join('images', 'images.id', '=', 'products.id_image')
-                            ->select('products.*', 'images.path')
-                            ->get();
+            ->joinSub($orderDetail, 'order_detail', function ($join) {
+                $join->on('products.id', '=', 'order_detail.product_id');
+            })
+            ->join('images', 'images.id', '=', 'products.id_image')
+            ->select('products.*', 'images.path')
+            ->get();
         $topNewProducts = DB::table('products')
-                            ->orderBy('id', 'desc')
-                            ->join('images', 'images.id', '=', 'products.id_image')
-                            ->select('products.*', 'images.path')
-                            ->take(10)
-                            ->get();
+            ->orderBy('id', 'desc')
+            ->join('images', 'images.id', '=', 'products.id_image')
+            ->select('products.*', 'images.path')
+            ->take(10)
+            ->get();
+        $topLikeProducts = DB::table('products')
+            ->orderBy('liked', 'desc')
+            ->join('images', 'images.id', '=', 'products.id_image')
+            ->select('products.*', 'images.path')
+            ->take(10)
+            ->get();
         return view('users.mainLayoutUser', [
             'categories' => $categories,
             'topNewProducts' => $topNewProducts,
-            'topSaleProducts' => $topSaleProducts
+            'topSaleProducts' => $topSaleProducts,
+            'topLikeProducts' => $topLikeProducts
         ]);
     }
 
-    public function DetailsProduct($pid) {
+    public function DetailsProduct($pid)
+    {
         $product = DB::table('products')
-                            ->where('products.id', $pid)
-                            ->take(1)
-                            ->get();
+            ->where('products.id', $pid)
+            ->take(1)
+            ->get();
 
         $images_product = DB::table('image_details_product')
-                        ->join('images', 'images.id', '=', 'image_details_product.id_image')
-                        ->where('image_details_product.id_product',  $pid)
-                        ->get();
+            ->join('images', 'images.id', '=', 'image_details_product.id_image')
+            ->where('image_details_product.id_product', $pid)
+            ->get();
 
         $information = DB::table('information')
-                    ->join('products', 'products.id_infomation', '=', 'information.id')
-                    ->where('products.id',  $pid)
-                    ->get();
+            ->join('products', 'products.id_infomation', '=', 'information.id')
+            ->where('products.id', $pid)
+            ->get();
 
         return view('users.products.productDetails', [
             'product' => $product,
             'images_product' => $images_product,
-            'information' =>  $information
+            'information' => $information
         ]);
     }
 
-    public function SearchProduct() {
+    public function SearchProduct()
+    {
         $key = request()->get('prd');
 
         $products = DB::table('products')
-                    ->where('products.products_name', 'like', '%'. $key.'%')
-                    ->join('images', 'images.id', '=', 'products.id_image')
-                    ->select('products.*', 'images.path')
-                    ->get();
+            ->where('products.products_name', 'like', '%' . $key . '%')
+            ->join('images', 'images.id', '=', 'products.id_image')
+            ->select('products.*', 'images.path')
+            ->get();
 
-        return view('users.search.searchPage', [ 'products'=>$products]);
+        return view('users.search.searchPage', ['products' => $products]);
     }
 }
